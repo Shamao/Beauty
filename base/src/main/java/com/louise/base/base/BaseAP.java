ba5d5fe8f49b4925d121102406d886335c54ece8
Liu Saideng <994516742@qq.com>
2018-03-22 15:22:50 +0800

    添加维护activity的堆栈

--- base/src/main/java/com/louise/base/base/BaseAP.java
+++ base/src/main/java/com/louise/base/base/BaseAP.java
@@ -1,11 +1,14 @@
 package com.louise.base.base;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 import android.util.Log;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.louise.base.BuildConfig;
 import com.louise.base.net.RetrofitManager;
+import com.louise.base.utils.ActivityStack;
 
 /**
  * @author lsd
@@ -13,6 +16,7 @@ import com.louise.base.net.RetrofitManager;
  */
 
 public class BaseAP extends Application {
+
 
     @Override
     public void onCreate() {
@@ -25,5 +29,42 @@ public class BaseAP extends Application {
         ARouter.init(this);
 
         RetrofitManager.getInstance().init(this);
+
+        registerActivityListener();
+    }
+
+
+    private void registerActivityListener() {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                ActivityStack.getInstance().addActivity(activity);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                ActivityStack.getInstance().removeActivity(activity);
+            }
+        });
     }
 }
---
+++ base/src/main/java/com/louise/base/utils/ActivityStack.java
@@ -0,0 +1,98 @@
+package com.louise.base.utils;
+
+import android.app.Activity;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author lsd
+ * @date 2018/3/22.
+ */
+
+public class ActivityStack {
+    private static final String S_TAG = "s_activity_stack";
+
+    private List<Activity> mActivityList;
+
+    public static class Holder {
+        public static ActivityStack instance = new ActivityStack();
+    }
+
+    public static ActivityStack getInstance() {
+        return Holder.instance;
+    }
+
+    private ActivityStack() {
+        mActivityList = Collections.synchronizedList(new LinkedList<Activity>());
+    }
+
+
+    public void addActivity(Activity activity) {
+        if (activity == null) {
+            return;
+        }
+        mActivityList.add(activity);
+        Logger.d(S_TAG, "addActivity", mActivityList.toString());
+    }
+
+    public void removeActivity(Activity activity) {
+        if (activity == null) {
+            return;
+        }
+        mActivityList.remove(activity);
+        Logger.d(S_TAG, "removeActivity", mActivityList.toString());
+    }
+
+    /**
+     * get top activity
+     *
+     * @return activity
+     */
+    public Activity getTopActivity() {
+        if (mActivityList == null || mActivityList.isEmpty()) {
+            return null;
+        }
+        return mActivityList.get(mActivityList.size() - 1);
+    }
+
+    public void finishActivity(Activity activity) {
+        if (mActivityList == null || mActivityList.isEmpty()) {
+            return;
+        }
+        if (activity == null) {
+            return;
+        }
+        removeActivity(activity);
+        activity.finish();
+    }
+
+
+    public boolean finishActivity(Class<? extends Activity> clz) {
+        if (mActivityList == null || mActivityList.isEmpty()) {
+            return false;
+        }
+
+        for (int i = 0; i < mActivityList.size(); i++) {
+            Activity activity = mActivityList.get(i);
+            if (activity.getClass() == clz) {
+                finishActivity(activity);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * check if all activity had bean finished
+     *
+     * @return true or false
+     */
+    public boolean isAllActivityFinished() {
+        return mActivityList == null || mActivityList.isEmpty();
+    }
+
+}
